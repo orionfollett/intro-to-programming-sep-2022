83e31d18063f2505da5e759d611795958be0f91d
Orion Follett <orionfollett@gmail.com>
2022-09-12 16:59:34 -0400

    initial

---
+++ Goals/Goals/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine("View previous entries?");
+
+
+Console.WriteLine("Enter your goal for today:");
+Console.WriteLine("--------------------------");
+
+string? goal = Console.ReadLine();
+
+Console.WriteLine($"\n[For {DateTime.Today} Your Goal is:]");
+
+if (goal != null)
+{
+    Console.WriteLine($"\n\"{goal}\"");
+}
+else {
+    Console.WriteLine("\nYour goal is null!");
+}
+
+Console.Write("\n[Save Changes (Y/n)] ");   
+string? resp = Console.ReadLine();
+
+if (resp != null) {
+    if (resp.Trim().ToLower().Equals("y"))
+    {
+        using StreamWriter file = new("WriteLines.txt", append: true);
+        await file.WriteLineAsync(goal);
+        Console.WriteLine("Saved");
+    }
+    else {
+        Console.WriteLine("Not Saved");
+    }
+}
